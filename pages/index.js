4ed70af754967c223431ef7aa56dc01e8c31b0de
Julian Vazquez <julianvazquez101@gmail.com>
2021-06-09 09:39:36 -0400

    Update index title

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,17 @@
+import Head from 'next/head';
 import Home from '../components/home-page/Home';
 
 export default function HomePage() {
-    return <Home />;
+    return (
+        <>
+            <Head>
+                <title>The Castle</title>
+                <meta
+                    name='viewport'
+                    content='initial-scale=1.0, width=device-width'
+                />
+            </Head>
+            <Home />
+        </>
+    );
 }
