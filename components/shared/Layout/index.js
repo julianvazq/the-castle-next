81f2d0a9dbf79ec10c413cff8fd35acc8dd53040
Julian Vazquez <julianvazquez101@gmail.com>
2021-06-26 13:01:12 -0400

    Add favicons

--- components/shared/Layout/index.js
+++ components/shared/Layout/index.js
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import { useRouter } from 'next/router';
 import React from 'react';
 import Footer from '../Footer';
@@ -10,6 +11,33 @@ const Layout = ({ children }) => {
     const router = useRouter();
     return (
         <S.AppContainer>
+            <Head>
+                <link
+                    rel='apple-touch-icon'
+                    sizes='76x76'
+                    href='/favicon/apple-touch-icon.png'
+                />
+                <link
+                    rel='icon'
+                    type='image/png'
+                    sizes='32x32'
+                    href='/favicon/favicon-32x32.png'
+                />
+                <link
+                    rel='icon'
+                    type='image/png'
+                    sizes='16x16'
+                    href='/favicon/favicon-16x16.png'
+                />
+                <link rel='manifest' href='/favicon/site.webmanifest' />
+                <link
+                    rel='mask-icon'
+                    href='/favicon/safari-pinned-tab.svg'
+                    color='#ffffff'
+                />
+                <meta name='msapplication-TileColor' content='#da532c' />
+                <meta name='theme-color' content='#ffffff' />
+            </Head>
             {router.pathname !== '/donate' ? <Hero /> : <NavBar />}
             {children}
             <NewsletterSignup />
